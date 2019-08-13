982c1d31dd8481607c86434a57e56609621a514a
wangzhijia <13311503035@163.com>
2019-08-13 12:49:18 +0800

    ssl

--- src/main/java/com/ruoyi/RuoYiApplication.java
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -35,20 +35,5 @@ public class RuoYiApplication
                 " ''-'   `'-'    `-..-'              ");
     }
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
-        return tomcat;
-    }
 
-    private Connector createHTTPConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        //同时启用http（80）、https（443）两个端口
-        connector.setScheme("http");
-        connector.setSecure(false);
-        connector.setPort(8080);
-        connector.setRedirectPort(443);
-        return connector;
-    }
 }
